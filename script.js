dac9e7fb3126ceb9685faafb83d78a8b63c422d4
thijs-den-hartog <43982636+thijs-den-hartog@users.noreply.github.com>
2019-06-13 09:49:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let knop = document.getElementById('knop')
+
+
+knop.addEventListener("click", myFunction)
+
+function myFunction() {
+  var uur = document.getElementById("uur").value;
+  var minuten = document.getElementById("minuten").value;
+  var seconden = document.getElementById("seconden").value;
+  var uur1 = document.getElementById("uur1")
+  var minuten1 = document.getElementById("minuten1")
+  var seconden1 = document.getElementById("seconden1")
+
+  uur1.innerHTML = uur;
+  minuten1.innerHTML = minuten;
+  seconden1.innerHTML = seconden;
+
+  var secondenTimer = seconden + minuten * 60  + uur * 3600;
+
+  while (true) {
+
+  }
+}
