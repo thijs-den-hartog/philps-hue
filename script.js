661b3002521fbbb10174716e2ec04ab533d6bdb6
Adam Klerkx <43774557+AdamKlerkx@users.noreply.github.com>
2019-06-13 10:42:25 +0200

    verder ge koomen
    het gaat beeter ik ben een paar bugs aan het op losen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let knop = document.getElementById('knop')
 
 
 knop.addEventListener("click", myFunction)
-
+alert("hallo");
 function myFunction() {
   var uur = document.getElementById("uur").value;
   var minuten = document.getElementById("minuten").value;
@@ -16,15 +16,18 @@ function myFunction() {
   seconden1.innerHTML = seconden;
 
   var secondenTimer = seconden + minuten * 60  + uur * 3600;
-  alert(secondenTimer)
-  let secondenTimer = timerseconde
-  alert(secondenTimer)
-  timerloop = true
-  while (timerloop) {
-    setTimeout(tijdmineen, 1000);
-    if {timerseconde <= 1}
-      timerloop = false
+
+}
+alert(secondenTimer);
+secondenTimer = timerseconde;
+alert(secondenTimer);
+timerloop = true;
+while (timerloop) {
+  setTimeout(tijdmineen, 1000);
+  if (timerseconde <= 1){
+    timerloop = false;
   }
   function tijdmineen() {
-    let timerseconde -=
+    timerseconde -= ;
+  }
 }
