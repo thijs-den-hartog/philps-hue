d4c26c4473b6d15dff20b6cfcb83b2396e00f825
Thijs den Hartog <Thijs.den.hartog@coderclass.nl>
2019-06-14 11:27:11 +0200

    veranderd

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let knop = document.getElementById('knop')
 
+lol
 
 knop.addEventListener("click", myFunction)
 
 function myFunction() {
-  var uur = document.getElementById("uur").value;
-  var minuten = document.getElementById("minuten").value;
-  var seconden = document.getElementById("seconden").value;
-  var uur1 = document.getElementById("uur1")
-  var minuten1 = document.getElementById("minuten1")
-  var seconden1 = document.getElementById("seconden1")
+  var uurInput = parseInt(document.getElementById("uurInput", 10).value);
+  var minutenInput = parseInt(document.getElementById("minutenInput", 10).value);
+  var secondenInput = parseInt(document.getElementById("secondenInput", 10).value);
+  var uurOutput = document.getElementById("uurOutput")
+  var minutenOutput = document.getElementById("minutenOutput")
+  var secondenOutput = document.getElementById("secondenOutput")
 
-  uur1.innerHTML = uur;
-  minuten1.innerHTML = minuten;
-  seconden1.innerHTML = seconden;
+  uurOutput.innerHTML = uurInput;
+  minutenOutput.innerHTML = minutenInput;
+  secondenOutput.innerHTML = secondenInput;
 
-  var secondenTimer = seconden + minuten * 60  + uur * 3600;
+  // var secondenTimer = seconden + minuten / 60  + uur / 3600;
+  var uurSecondes = uurInput *60 *60;
+  var minutenSecondes = minutenInput *60;
+  var secondenTimer = uurSecondes + minutenSecondes + secondenInput;
+
+
+  alert(secondenTimer);
 
   while (true) {
 
