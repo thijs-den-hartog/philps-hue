6e24e7b2a307998c3fcc7bf0a0174e7fd97066e9
Thijs den Hartog <Thijs.den.hartog@coderclass.nl>
2019-06-21 10:04:28 +0200

    laatste veranderingen

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ lol
 knop.addEventListener("click", myFunction)
 
 function myFunction() {
-  var uurInput = parseInt(document.getElementById("uurInput", 10).value);
-  var minutenInput = parseInt(document.getElementById("minutenInput", 10).value);
-  var secondenInput = parseInt(document.getElementById("secondenInput", 10).value);
-  var uurOutput = document.getElementById("uurOutput")
-  var minutenOutput = document.getElementById("minutenOutput")
-  var secondenOutput = document.getElementById("secondenOutput")
+  let uurInput = parseInt(document.getElementById("uurInput", 10).value);
+  let minutenInput = parseInt(document.getElementById("minutenInput", 10).value);
+  let secondenInput = parseInt(document.getElementById("secondenInput", 10).value);
+  let uurOutput = document.getElementById("uurOutput")
+  let minutenOutput = document.getElementById("minutenOutput")
+  let secondenOutput = document.getElementById("secondenOutput")
 
-  uurOutput.innerHTML = uurInput;
-  minutenOutput.innerHTML = minutenInput;
-  secondenOutput.innerHTML = secondenInput;
+  
 
   // var secondenTimer = seconden + minuten / 60  + uur / 3600;
   var uurSecondes = uurInput *60 *60;
@@ -25,6 +23,8 @@ function myFunction() {
   alert(secondenTimer);
 
   while (true) {
-
+    uurOutput.innerHTML = uurInput;
+    minutenOutput.innerHTML = minutenInput;
+    secondenOutput.innerHTML = secondenInput;
   }
 }
